02edacf6748e42781ed7a9ddd361b22b53ab2b98
BarZamsky <barzamsky1@gmail.com>
2019-08-02 22:21:00 +0300

    bug fix

--- server/index.js
+++ server/index.js
@@ -18,10 +18,6 @@ app.use(fileUpload());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.post('/api/upload', (req, res, next) => {
   let uploadFile = req.files.file
   const fileName = req.files.file.name
